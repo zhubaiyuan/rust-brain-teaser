ce7e51008b34c94b159cd133bc7cbfbaa6f627ee
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-05-04 21:17:02 +0800

    shadowing

---
+++ p15_reverse_polarity/src/main.rs
@@ -0,0 +1,17 @@
+fn display_neutron_flow(polarity: isize) {
+    println!(
+        "Neutron Flow is {}",
+        if polarity < 0 { "reversed" } else { "normal" }
+    );
+}
+
+fn main() {
+    let polarity = 1;
+    {
+        let polarity = polarity - 2;
+        display_neutron_flow(polarity);
+    }
+    display_neutron_flow(polarity);
+}
+// Neutron Flow is reversed
+// Neutron Flow is normal
