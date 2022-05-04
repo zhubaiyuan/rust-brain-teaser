e5b6dcddaaa78adcb581d7d1ed2d4b59bce26bc7
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-05-04 21:43:10 +0800

    vector growth

---
+++ p20_vec_size/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut my_vec = Vec::with_capacity(1);
+    my_vec.push("Hello");
+    println!("{}", my_vec.capacity());
+    my_vec.push("World");
+    println!("{}", my_vec.capacity());
+}
+// 1
+// 4
