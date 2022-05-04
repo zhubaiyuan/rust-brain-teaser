ada0d7d99cda90a98dc69c848057458d41b4aa02
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-05-04 21:48:40 +0800

    mutably borrowed variables

---
+++ p21_mutable_immutable/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let life_the_universe = &mut 41;
+    *life_the_universe += 1;
+    println!("Life, the Universe and Everything: {}", life_the_universe);
+}
+// Life, the Universe and Everything: 42
