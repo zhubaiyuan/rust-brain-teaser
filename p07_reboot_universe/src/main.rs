a095fde985fb4fcf6272f86ea913323c64d0d98b
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-05-04 17:51:57 +0800

    EPSILON is built-in to each of Rust’s floating-point types

--- p07_reboot_universe/src/main.rs
+++ p07_reboot_universe/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    if 0.1 + 0.2 == 0.3 {
+    // if 0.1 + 0.2 == 0.3 {
+    if (0.1f32 + 0.2f32 - 0.3f32).abs() < std::f32::EPSILON {
         println!("Arithmetic still works.");
     } else {
         println!("Please reboot the universe.");
     }
 }
-// Please reboot the universe.
