b9ab4cc51b21fa57fe27cf40f6b0425b8a00ca8b
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-05-04 17:43:00 +0800

    translate the string to utf-8

---
+++ p06_string_length/src/main.rs
@@ -0,0 +1,6 @@
+const HELLO_WORLD: &'static str = "Halló heimur";
+
+fn main() {
+    println!("{} is {} characters long.", HELLO_WORLD, HELLO_WORLD.len());
+}
+// Halló heimur is 13 characters long.
