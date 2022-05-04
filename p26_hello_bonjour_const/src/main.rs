fe04ca3302d7ba27ffefb2b80d46275a3012acee
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-05-04 22:08:31 +0800

    hello, bonjour

---
+++ p26_hello_bonjour_const/src/main.rs
@@ -0,0 +1,15 @@
+enum Language {
+    English,
+    French,
+}
+
+const fn hello(language: Language) -> &'static str {
+    match language {
+        Language::English => "Hello World",
+        Language::French => "Bonjour le monde",
+    }
+}
+
+fn main() {
+    println!("{}", hello(Language::English));
+}
