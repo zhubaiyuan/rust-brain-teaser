737164f5f93e0627ce1e3caafad936026f1117e5
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-05-04 22:00:14 +0800

    native threads

---
+++ p23_async_threaded/src/main.rs
@@ -0,0 +1,25 @@
+use std::thread;
+use std::time::Duration;
+
+fn count_and_wait(n: u64) -> u64 {
+    println!("Starting {}", n);
+    std::thread::sleep(Duration::from_millis(n * 100));
+    println!("Returning {}", n);
+    n
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let a = thread::spawn(|| count_and_wait(1));
+    let b = thread::spawn(|| count_and_wait(2));
+    let c = thread::spawn(|| count_and_wait(3));
+    a.join().unwrap();
+    b.join().unwrap();
+    c.join().unwrap();
+    Ok(())
+}
+// Starting 1
+// Starting 2
+// Starting 3
+// Returning 1
+// Returning 2
+// Returning 3
