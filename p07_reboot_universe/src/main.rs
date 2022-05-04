87e65e5dd2b37b5cdfebb246470f622b32e833e3
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-05-04 17:48:39 +0800

    never compare floats

---
+++ p07_reboot_universe/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    if 0.1 + 0.2 == 0.3 {
+        println!("Arithmetic still works.");
+    } else {
+        println!("Please reboot the universe.");
+    }
+}
+// Please reboot the universe.
